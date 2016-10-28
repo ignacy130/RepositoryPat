0b1aeb181314bf685987de5680728cd707286157
ignacy130@gmail.com <ignacy130@gmail.com>
2016-10-28 11:01:28 +0200

    course controler and department entity

---
+++ src/RepositoryPat/Controllers/CourseController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pat.Domain.DAL;
+using Microsoft.EntityFrameworkCore;
+using Pat.Domain.Models;
+using RepositoryPat.DAL;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RepositoryPat.Controllers
+{
+    public class CourseController : Controller
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
+        public ViewResult Index()
+        {
+            var courses = unitOfWork.CourseRepository.Get();
+            return View(courses.ToList());
+        }
+
+        public ViewResult Details(int id)
+        {
+            Course course = unitOfWork.CourseRepository.GetByID(id);
+            return View(course);
+        }
+
+        public ActionResult Create()
+        {
+            PopulateDepartmentsDropDownList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("Id,Title,Credits,DepartmentID")] Course course)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    unitOfWork.CourseRepository.Insert(course);
+                    unitOfWork.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException /* dex */)
+            {
+                //Log the error (uncomment dex variable name after DataException and add a line here to write a log.)
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+            PopulateDepartmentsDropDownList(course.DepartmentId);
+            return View(course);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Course course = unitOfWork.CourseRepository.GetByID(id);
+            PopulateDepartmentsDropDownList(course.DepartmentId);
+            return View(course);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind("Id,Title,Credits,DepartmentID")]
+         Course course)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    unitOfWork.CourseRepository.Update(course);
+                    unitOfWork.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException /* dex */)
+            {
+                //Log the error (uncomment dex variable name after DataException and add a line here to write a log.)
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+            PopulateDepartmentsDropDownList(course.DepartmentId);
+            return View(course);
+        }
+
+        private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
+        {
+            var departmentsQuery = unitOfWork.DepartmentRepository.Get(orderBy: q => q.OrderBy(d => d.Name));
+            ViewBag.DepartmentID = new SelectList(departmentsQuery, "DepartmentID", "Name", selectedDepartment);
+        }
+
+        //
+        // GET: /Course/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Course course = unitOfWork.CourseRepository.GetByID(id);
+            return View(course);
+        }
+
+        //
+        // POST: /Course/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Course course = unitOfWork.CourseRepository.GetByID(id);
+            unitOfWork.CourseRepository.Delete(id);
+            unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            unitOfWork.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ src/RepositoryPat/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿using RepositoryPat.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pat.Domain.Models
+{
+    public class Department : EntityBase
+    {
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Budget { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Administrator")]
+        public int? InstructorID { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
