ada6621b8f93b4e15f7970a104fee7746bacc398
ignacy130@gmail.com <ignacy130@gmail.com>
2016-11-03 13:19:16 +0100

    working dashboard view

--- src/RepositoryPat/wwwroot/app/dashboard.component.ts
+++ src/RepositoryPat/wwwroot/app/dashboard.component.ts
@@ -1,7 +1,23 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+
+import { Student } from './student';
+import { StudentService } from './student.service';
 
 @Component({
     selector: 'dashboard',
-    template: '<h3>Dashboard</h3>'
+    templateUrl: 'app/dashboard.component.html',
 })
-export class DashboardComponent { }
+
+export class DashboardComponent implements OnInit {
+
+    students: Student[] = [];
+
+    constructor(private studentsService: StudentService) { }
+
+    ngOnInit(): void {
+        this.studentsService.getStudents()
+            .then(students => this.students = students.slice(1, 5));
+    }
+
+    gotoDetail(student: Student): void { /* not implemented yet */ }
+}
